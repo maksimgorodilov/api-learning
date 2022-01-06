d537cec9f65677ea5c35da4ea22b424e6b61b60c
mgorodilov <maksim.gorodilov@ah.nl>
2022-01-06 15:16:49 +0100

    chore: initial springboot commit

---
+++ src/main/kotlin/com/matim/apimanagementexample/apimanagementexample/ApiManagementExampleApplication.kt
@@ -0,0 +1,11 @@
+package com.matim.apimanagementexample.apimanagementexample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiManagementExampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiManagementExampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/matim/apimanagementexample/apimanagementexample/ApiManagementExampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.matim.apimanagementexample.apimanagementexample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiManagementExampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

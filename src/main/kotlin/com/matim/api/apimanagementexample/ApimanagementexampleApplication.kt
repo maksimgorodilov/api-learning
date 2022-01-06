c253b51f4e8999ab0b1c754a0558da4446bb1f5c
mgorodilov <maksim.gorodilov@ah.nl>
2022-01-06 16:56:54 +0100

    war config

--- src/main/kotlin/com/matim/api/apimanagementexample/ApimanagementexampleApplication.kt
+++ src/main/kotlin/com/matim/api/apimanagementexample/ApimanagementexampleApplication.kt
@@ -1,10 +1,17 @@
 package com.matim.api.apimanagementexample
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
 
 @SpringBootApplication
-class ApimanagementexampleApplication
+class ApimanagementexampleApplication: SpringBootServletInitializer() {
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder? {
+		return application.sources(ApimanagementexampleApplication::class.java)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ApimanagementexampleApplication>(*args)
